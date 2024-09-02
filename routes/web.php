a6fae4f117568c5ca71c89094c66ba1c341d8a4c
aku3aji <triaji3ibnu@gmail.com>
2024-09-02 10:23:32 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/hello', function () {
+    return 'Hello World';
+});   
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/world', function () {
+    return 'World';
 });
+
+Route::get('/salam', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Hallo! Saya Triaji Ibnu Hermawan dengan NIM 2241760075';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+    
